2e64bcf2039e3093ec2eb11b76245a27d5ea0c2c
engstk <eng.stk@sapo.pt>
2017-07-31 18:28:24 +0100

    oplus_chg: charger: Implement usb2 fast charge mode
    @neobuddy89:
    This is stripped down version of actual patch to make it
    compile with oplus_charger as module.
    
    Enables force charging up to 900mA in usb2 mode
    
    echo 0 /sys/module/oplus_chg/parameters/force_fast_charge (disable)
    echo 1 /sys/module/oplus_chg/parameters/force_fast_charge (enable)
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: amit-0i <amit0i+github@proton.me>
    Signed-off-by: viaan-thakur <viaan4542@gmail.com>

--- drivers/power/oplus/oplus_charger.c
+++ drivers/power/oplus/oplus_charger.c
@@ -238,6 +238,12 @@ static int oplus_chg_gauge_update_check(struct oplus_chg_chip *chip, bool ffc_st
 extern int sub_gauge_dbg_tbat;
 extern int gauge_dbg_tbat;
 
+static int force_fast_charge = 0;
+module_param(force_fast_charge, int, 0644);
+
+static int ffc_val = 900;
+module_param(ffc_val, int, 0644);
+
 static int chgr_dbg_vchg = 0;
 module_param(chgr_dbg_vchg, int, 0644);
 MODULE_PARM_DESC(chgr_dbg_vchg, "debug charger voltage");
@@ -6157,7 +6163,11 @@ void oplus_chg_set_input_current_limit(struct oplus_chg_chip *chip)
 	case POWER_SUPPLY_TYPE_UNKNOWN:
 		return;
 	case POWER_SUPPLY_TYPE_USB:
-		current_limit = chip->limits.input_current_usb_ma;
+		if (force_fast_charge > 0) {
+			current_limit = ffc_val;
+		} else {
+			current_limit = chip->limits.input_current_usb_ma;
+		}
 		break;
 	case POWER_SUPPLY_TYPE_USB_DCP:
 		current_limit = chip->limits.input_current_charger_ma;
